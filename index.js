b87663a0fc7e9c171c7340a13486525854956ab1
fp <fp@gmail.com>
2023-01-12 10:23:30 +0800

    加密文件

--- index.js
+++ index.js
@@ -53,21 +53,35 @@ const config = {
             version: "",
             selectedApp: 0,
             selectedEnv: 0,
+            key: CryptoJS.enc.Utf8.parse("1234123412ABCDEF"),
+            iv: CryptoJS.enc.Utf8.parse("ABCDEF1234123412")
         }
     },
     methods: {
+        downloadApp() {
+            window.location.href = this.downLink
+        },
+        
+        Decrypt(word) {
+            let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+            let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+            let decrypt = CryptoJS.AES.decrypt(srcs, this.key, { iv: this.iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+            let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+            return decryptedStr.toString();
+        },
     },
     computed: {
         buttonTitle() {
             const app = this.appList[this.selectedApp]
             const env = this.envList[this.selectedEnv]
-
             return `下载${app.name}-${env.name}-v${this.version}`
         },
         downLink() {
             const app = this.appList[this.selectedApp]
             const env = this.envList[this.selectedEnv]
-            return `itms-services://?action=download-manifest&url=https://appdownload.qiyou.cn/${app.code}-v${this.version}-${env.code}.plist`
+            const a = "BFDCBD92C944F09A10DA18807635CFFA43C30592E6CA13A6812186092B8E9D2C325AC528E82284877AED51D148D8E4B28C6FA878EB2619C5C5E63D969F259E6513509006E0F8838464749ED35C65A20E"
+            const b = "27E201FEC269891FA04242D78F12BDA4"
+            return `${this.Decrypt(a)}${app.code}-v${this.version}-${env.code}${this.Decrypt(b)}`
         }
     }
 }
